cae35d99b0aed4104652962650ce8c57f045514d
Julian Sehne <JulianM.sehne@gmail.com>
2025-12-07 13:28:32 +0100

    Add Training loop to main

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import torch
+import argparse
 
-from data_loading.load_data import train_val_test_split, DatasetConfig
+import torch
+from torch.utils.data import DataLoader
+
+from data_loading.load_data import train_val_test_split, DatasetConfig, load_single_recording
 from data_loading.pose_landmark import PoseLandmark
 from model.pose_estimation_model import RegressionModel
 from model.sensfloor_trainer import SensfloorTrainer
 from model.utils import set_seed
 
+from pathlib import Path
 
-def main() -> None:
+
+def main(do_train: bool, do_test: bool) -> None:
     set_seed(seed=42)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     device = torch.device(device)
 
+    model_path = Path("./model/best_model.pth")
 
     drop_landmarks = [PoseLandmark.LEFT_EYE, PoseLandmark.LEFT_EYE_INNER, PoseLandmark.LEFT_EYE_OUTER,
                       PoseLandmark.RIGHT_EYE, PoseLandmark.RIGHT_EYE_INNER, PoseLandmark.RIGHT_EYE_OUTER,
@@ -20,22 +26,43 @@ def main() -> None:
                       PoseLandmark.LEFT_THUMB, PoseLandmark.LEFT_INDEX, PoseLandmark.LEFT_PINKY,
                       PoseLandmark.RIGHT_INDEX, PoseLandmark.RIGHT_THUMB, PoseLandmark.RIGHT_PINKY]
 
-
     dataset_config = DatasetConfig(roi_size=3, floor_size_x=6, floor_size_y=4, history_maxlen=10,
                                    drop_landmarks=drop_landmarks)
-    train_loader, val_loader, test_loader = train_val_test_split(ratios=(0.7, 0.10, 0.20), config=dataset_config)
-    rois, poses = next(iter(train_loader))
-    print(f"rois: {rois}")
 
     roi_shape = (dataset_config.roi_size * 4, dataset_config.roi_size * 4)
     landmarks_out = len(PoseLandmark) - len(drop_landmarks)
-    model = RegressionModel(roi_shape=roi_shape, landmarks_out=landmarks_out, history_len=dataset_config.history_maxlen)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.1)
+    if do_train:
+        train_loader, val_loader, test_loader = train_val_test_split(ratios=(0.7, 0.10, 0.20), config=dataset_config)
 
-    trainer = SensfloorTrainer(model=model, device=device, optimizer=optimizer, patience=5, use_early_stopping=True)
-    trainer.train(train_loader=train_loader, validation_loader=val_loader, epochs=10)
+        model = RegressionModel(roi_shape=roi_shape, landmarks_out=landmarks_out, history_len=dataset_config.history_maxlen)
+
+        optimizer = torch.optim.AdamW(model.parameters(), lr=0.1)
+
+        trainer = SensfloorTrainer(model=model, device=device, optimizer=optimizer, patience=5, use_early_stopping=True)
+        trainer.train(train_loader=train_loader, validation_loader=val_loader, epochs=10)
+
+    if do_test:
+
+        dataset = load_single_recording(Path("./data/2025-12-02_12-30-55"), config=dataset_config)
+        dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+        model = torch.load(f=model_path)
+
+        for roi_history, pose in dataloader:
+            outputs = model(roi_history)
+
+
+
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--train", action="store_true", help="Include flag if model should train")
+    parser.add_argument("--test", action="store_true", help="Include flag if model should create test predictions")
+
+    args = parser.parse_args()
+
+    main(args.train, args.test)
--- model/sensfloor_trainer.py
+++ model/sensfloor_trainer.py
@@ -38,10 +38,9 @@ class SensfloorTrainer(BaseTrainer):
         return self.loss(outputs, labels)
 
     def calculate_accuracy(self, outputs, labels, threshold=0.1):
-        coords = outputs.view(-1, 17, 3)  # [B, 21, 3] #TODO: parametrize landmarks_out
-        reshaped_labels = labels.view(-1, 17, 3)  # [B, 21, 3] #TODO: parametrize landmarks_out
-        dist = torch.linalg.vector_norm(coords - reshaped_labels, dim=2)  # [B, 21]
+        coords = outputs.view(-1, 17, 3)  # [B, 17, 3] #TODO: parametrize landmarks_out
+        reshaped_labels = labels.view(-1, 17, 3)  # [B, 17, 3] #TODO: parametrize landmarks_out
+        dist = torch.linalg.vector_norm(coords - reshaped_labels, dim=2)  # [B, 17]
         correct = (dist < threshold)
-        accuracy = correct.float().mean()  # average over all B × 21
-
-        return accuracy
+        accuracy = correct.float().mean()  # average over all B × 17
+        return accuracy * 100
