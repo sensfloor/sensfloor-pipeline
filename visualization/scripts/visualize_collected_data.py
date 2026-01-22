79fde1e973b847285669ec6df79577461c64ff7b
felixbauer <bauerfe84318@th-nuernberg.de>
2026-01-22 10:21:11 +0100

    Configure arguments for visualize collected data script

--- visualization/scripts/visualize_collected_data.py
+++ visualization/scripts/visualize_collected_data.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import cv2
@@ -5,68 +6,93 @@ import pandas as pd
 
 from data_loading.roi_floor import RoIFloor, RoIFloorConfig
 
-DATA_DIR_PATH = Path("data/2025-12-16_20-45-38-line-felix")
-READOUT_PATH = DATA_DIR_PATH / "sensfloor_readout.csv"
-VIDEO_PATH = DATA_DIR_PATH / "video.mp4"
 
-SIGNAL_THRESHOLD = 145
+def main(data_dir: Path, signal_threshold: int) -> None:
+    readout_path = data_dir / "sensfloor_readout.csv"
+    video_path = data_dir / "video.mp4"
 
-readout_df = pd.read_csv(READOUT_PATH)
+    readout_df = pd.read_csv(readout_path)
 
-display_w = 400
-display_h = 600
+    display_w = 400
+    display_h = 600
 
-cv2.namedWindow("Floor Heatmap", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Floor Heatmap", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
 
-cv2.resizeWindow("Floor Heatmap", display_w, display_h)
-cv2.resizeWindow("Video", 640, 480)
+    cv2.resizeWindow("Floor Heatmap", display_w, display_h)
+    cv2.resizeWindow("Video", 640, 480)
 
-cap = cv2.VideoCapture(str(VIDEO_PATH))
-fps = cap.get(cv2.CAP_PROP_FPS)
-wait_time = int(1000 / fps)
+    cap = cv2.VideoCapture(str(video_path))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    wait_time = int(1000 / fps)
 
-frame_number = 0
-floor_config = RoIFloorConfig(
-    x_size=6, y_size=4, history_maxlen=10, roi_size=3, active_field_min_value=SIGNAL_THRESHOLD
-)
-floor = RoIFloor(floor_config)
+    frame_number = 0
+    floor_config = RoIFloorConfig(
+        x_size=6,
+        y_size=4,
+        history_maxlen=10,
+        roi_size=3,
+        active_field_min_value=signal_threshold,
+    )
+    floor = RoIFloor(floor_config)
 
-while True:
-    ret, frame = cap.read()
-    messages = readout_df[readout_df["frame_number"] == frame_number]
-    positions = messages[["x", "y"]].to_numpy() - 1
-    signals = messages[["0", "1", "2", "3", "4", "5", "6", "7"]].to_numpy()
-    signals[signals < SIGNAL_THRESHOLD] = 0
-    floor.update(positions=positions, signals=signals)
-    current_floor = floor.history[-1].astype("uint8")
-    roi = floor.get_roi()
-    current_floor[current_floor < floor_config.active_field_min_value] = 0
+    while True:
+        ret, frame = cap.read()
+        messages = readout_df[readout_df["frame_number"] == frame_number]
+        positions = messages[["x", "y"]].to_numpy() - 1
+        signals = messages[["0", "1", "2", "3", "4", "5", "6", "7"]].to_numpy()
+        signals[signals < signal_threshold] = 0
+        floor.update(positions=positions, signals=signals)
+        current_floor = floor.history[-1].astype("uint8")
+        roi = floor.get_roi()
+        current_floor[current_floor < floor_config.active_field_min_value] = 0
 
-    display_img = cv2.cvtColor(current_floor, cv2.COLOR_GRAY2BGR)
-    if roi is not None:
-        x = roi.x * 4
-        y = roi.y * 4
-        size = floor.roi_size
+        display_img = cv2.cvtColor(current_floor, cv2.COLOR_GRAY2BGR)
+        if roi is not None:
+            x = roi.x * 4
+            y = roi.y * 4
+            size = floor.roi_size
 
-        cv2.rectangle(
-            display_img,
-            (y, x),
-            (y + size * 4 - 1, x + size * 4 - 1),
-            (0, 0, 255),
-            1,
-        )
+            cv2.rectangle(
+                display_img,
+                (y, x),
+                (y + size * 4 - 1, x + size * 4 - 1),
+                (0, 0, 255),
+                1,
+            )
 
-    heatmap_resized = cv2.resize(display_img, (display_w, display_h), interpolation=cv2.INTER_NEAREST)
-    cv2.imshow("Floor Heatmap", heatmap_resized)
+        heatmap_resized = cv2.resize(display_img, (display_w, display_h), interpolation=cv2.INTER_NEAREST)
+        cv2.imshow("Floor Heatmap", heatmap_resized)
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    cv2.imshow("Video", frame)
-    frame_number += 1
-    if cv2.waitKey(wait_time) & 0xFF == ord("q"):
-        break
+        cv2.imshow("Video", frame)
+        frame_number += 1
+        if cv2.waitKey(wait_time) & 0xFF == ord("q"):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Visualize collected video and readout")
+    parser.add_argument(
+        "--data",
+        type=Path,
+        required=True,
+        help="Required path to data directory containing video and sensfloor readout.",
+    )
+    parser.add_argument(
+        "--signal-threshold",
+        type=int,
+        default=145,
+        help="Threshold value for signal being not considered as noise",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.data, args.signal_threshold)
