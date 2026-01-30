6abd997884731a75a4c9ed1127c77904951556c3
Julian Sehne <JulianM.sehne@gmail.com>
2026-01-30 12:38:23 +0100

    Add lstm dataset todos

--- training/dataset/lstm_dataset.py
+++ training/dataset/lstm_dataset.py
@@ -21,15 +21,14 @@ class LSTMDataset(SensfloorPosesDataset):
         )
         fig, ax = plt.subplots()
         ax.boxplot([self.sequences])
-        ax.set_xticklabels([f"{len(self.sequences)}"])
+        ax.set_xticklabels([f"{len(self.sequences)}"]) #TODO: Remove boxplotting
         plt.show()
 
     def __len__(self) -> int:
         return len(self.sequences)
 
     def get_detailed_data(self, index: int) -> DetailedSensfloorPosesData:
-        # Get signal history
-        frame_number, history_len = self.sequences[index]
+        frame_number, history_len = self.sequences[index]  #TODO: Return and load sequences with correct length
         old = self.config.floor_config
         floor = create_roi_floor(
             RoIFloorConfig(history_maxlen=history_len, x_size=old.x_size, y_size=old.y_size, roi_size=old.roi_size),
