896eadf66a0b5e93a026bdac26af0811439226c9
felixbauer <bauerfe84318@th-nuernberg.de>
2026-02-03 19:35:02 +0100

    Add message validator to data collection script

--- data_collection/scripts/collect_sensfloor_data_and_video.py
+++ data_collection/scripts/collect_sensfloor_data_and_video.py
@@ -7,6 +7,7 @@ from zoneinfo import ZoneInfo
 
 import cv2
 
+from data_collection.message_validator import PositionValidator
 from data_collection.messages_provider.serial_messages_provider import SerialMessagesProvider
 
 
@@ -54,7 +55,8 @@ def main(readout_dir: Path, serial_port: str, fps: int) -> None:
     writer.writeheader()
 
     # Start messages thread that writes messages in queue
-    messages_provider = SerialMessagesProvider(serial_port)
+    message_validator = PositionValidator(min_x=1, max_x=6, min_y=1, max_y=4)
+    messages_provider = SerialMessagesProvider(serial_port, message_validator=message_validator)
     messages_provider.start()
     recording_start_time = time.perf_counter()
     try:
